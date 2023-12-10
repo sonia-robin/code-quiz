73f37399f596a71fc3b2fe5836ed0138a476cd21
unknown <cervinkova.sonik@gmail.com>
2023-12-10 16:36:46 +0000

    questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+// create an array of objects
+
+var questions = [
+    {
+        question: "Commonly used data types do not include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within_____ .",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: ""
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/ bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        question: "Arrays can be used to store _____ .",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    }
+]
+
